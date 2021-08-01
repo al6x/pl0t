d96897a46de7ebb89d163a2ef12260596689b0be
alexeypetrushin <alexey.petrushin@gmail.com>
2021-08-02 01:12:30 +0300

    upd

---
+++ api/groovy/Page.groovy
@@ -0,0 +1,110 @@
+import groovy.json.JsonOutput
+import java.io.IOException
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class Page {
+  String title
+  String desc
+  def    page = []
+
+  def text(String id, String text) {
+    page.add([ id: id, text: text ])
+  }
+
+  def table(String id, data, table) {
+    page.add([ id: id, table: table, data: data ])
+  }
+
+  def chart(String id, data, chart) {
+    page.add([ id: id, chart: chart, data: data ])
+  }
+
+  def image(String id, data) {
+    page.add([ id: id, data: data ])
+  }
+
+  def save(String path) {
+    def html = standalone.replace('{type}', 'json').replace('{data}', toJson())
+    new File(path).withWriter('utf-8') {
+      writer -> writer.write html
+    }
+  }
+
+  def publish(String url) {
+    HttpClient client = HttpClient.newHttpClient()
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(url))
+      .header('api_token', Page.api_token())
+      .POST(HttpRequest.BodyPublishers.ofString(toJson()))
+      .build()
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    if (response.statusCode() != 200) throw new Exception("can't publish $url")
+  }
+
+  def static unpublish(String url) {
+    HttpClient client = HttpClient.newHttpClient()
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(url))
+      .header('api_token', Page.api_token())
+      .DELETE()
+      .build()
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    if (response.statusCode() != 200) throw new Exception("can't unpublish $url")
+  }
+
+  def protected toJson(String path) {
+    JsonOutput.toJson([ title: title, desc: desc, page: page ])
+  }
+
+  def static api_token() {
+    def api_token = System.getenv("plot_api_token")
+    if (!api_token) throw new Exception("the 'plot_api_token' env variable is not defined")
+    api_token
+  }
+
+
+// standalone begin
+String standalone = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <title>pl0t</title>
+
+  <link rel="icon" type="image/png" href="http://files.pl0t.com/view-1/favicon.ico">
+
+  <!-- PL0T begin, putting it at the end of the page to avoid blocking other content -->
+  <script>
+    window.env = {
+      base_url: "http://files.pl0t.com/view-1"
+    }
+  </script>
+  <link rel="stylesheet" href="http://files.pl0t.com/view-1/releases/2021-08-01-6fb7d9/bundle.css">
+  <script defer src="http://files.pl0t.com/view-1/releases/2021-08-01-6fb7d9/bundle.js"></script>
+  <!-- PL0T end -->
+
+</head>
+<body>
+
+<!-- PL0T data, specify 'json', 'yml' or 'md' formats in the 'type' attribute -->
+<script id="data" type="{type}">
+{data}
+</script>
+
+
+<script>
+  window.on_plot_loaded = function() {
+    window.plot_api.run()
+  }
+</script>
+
+</body>
+</html>
+"""
+// standalone end
+}
+
---
+++ api/groovy/play.groovy
@@ -0,0 +1,54 @@
+def page = new Page(title: 'Some page', desc: 'Some description')
+
+
+page.text('Some text', """
+Some formula \$E=mc^2\$
+
+Some code `puts 'Hello World'`
+""")
+
+
+// Data as TidyData or Array or Column Array
+def table_data = [
+  [ name: 'Jim Raynor',   age: 30, hp: 250,  is_alive: true ],
+  [ name: 'Angus Mengsk', age: 50, hp: 100,  is_alive: false ],
+  [ name: 'Amon',                  hp: 500,  is_alive: true ]
+]
+page.table('Some table', table_data, [
+  columns: [
+    [ id: 'name' ],
+    [ id: 'age' ],
+    [ id: 'hp', format: [ type: 'line', ticks: [100] ] ],
+    [ id: 'is_alive' ]
+  ]
+])
+
+
+// Data as TidyData or Array or Column Array
+chart_data = [
+  a: [1, 2, 3,  4, 5],
+  b: [1, 3, 2, -1, 2]
+]
+page.chart('Some chart', chart_data, [
+  'bar',
+  [ x: 'a', type: 'nominal' ],
+  [ y: 'b' ]
+])
+
+
+// page.image('Some image', [
+//   // Tiny black pixel encoded as base64 image.
+//   // Also url could be supplied as `url: 'http://some-url'`.
+//   base64: 'R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs='
+// ])
+
+
+// Saving report as HTML file, open it the Browser to see the Notebook
+// You can publish Notebook by copying it to any Web Server
+// page.save('play.html')
+
+
+// Optionally, you can publish Notebook on the http://pl0t.com site.
+// You would need to get API Token from http://pl0t.com and store it as `plot_api_token` env variable
+// The Notebook will be available as http://al6x.pl0t.com/groovy_test/page.json:view
+page.publish('http://al6x.pl0t.com/groovy_test/page.json')
