a68c2033df9f6692d388505c98db0d55d77d11e8
alexeypetrushin <alexey.petrushin@gmail.com>
2021-08-02 23:42:37 +0300

    upd

---
+++ api/ruby/pl0t.rb
@@ -0,0 +1,118 @@
+class Page
+  @app = ['page', 0.1]
+
+  attr_accessor :id, :title, :desc, :css, :page
+
+  def initialize params = {}
+    self.page = []
+    params.each do |k, v|
+      send("#{k}=", v)
+    end
+  end
+
+  def text id, text
+    page << { id: id, text: text }
+  end
+
+  def table id, data, table
+    page << { id: id, table: table, data: data }
+  end
+
+  def chart id, data, chart
+    page << { id: id, chart: chart, data: data }
+  end
+
+  def image id, data
+    page << { id: id, data: data }
+  end
+
+  def to_html
+    # Ruby emits non standard YAML that JavaScript can't parse. If you know how to make it to
+    # output standard YAML, please fix it, as YAML is better format than JSON.
+    #
+    # require 'yaml'
+    # Removing first line in YAML, ruby puts "--- !ruby/object:Page" there.
+    # data = to_yaml.sub(/^---[^\n]+\n/, '')
+
+    require 'json'
+    # For strange reason `self.to_json` doesn't work, iterating over instance variables
+    hash = {}
+    instance_variables.each {|v| hash[v.to_s.delete("@")] = instance_variable_get(v) }
+    data = hash.to_json
+
+    @@standalone.sub('{type}', 'json').sub('{data}', data)
+  end
+
+  def save path
+    File.write path, to_html
+  end
+
+  def publish url
+    require 'net/http'
+    require "uri"
+
+    api_token = ENV['plot_api_token']
+    raise "`plot_api_token` env variable is not set" if api_token.nil?
+    resp = Net::HTTP.post URI(url), to_html, { api_token: api_token }
+    raise "can't publish #{url}" unless resp.code == "200"
+  end
+
+  def self.unpublish url
+    require 'net/http'
+    require "uri"
+
+    api_token = ENV['plot_api_token']
+    raise "`plot_api_token` env variable is not set" if api_token.nil?
+
+    # I don't know how to make HTTP DELETE request with Ruby, the code below doesn't work
+    #
+    # resp = Net::HTTP.delete URI(url, { api_token: api_token })
+    # raise "can't delete #{url}" unless resp.code == "200"
+
+    resp = `curl -s --request DELETE --header "api_token: #{api_token}" "#{url}"`
+    raise "can't delete #{url}" unless /file deleted/ =~ resp
+  end
+
+
+  # standalone begin
+  @@standalone = <<~HTML
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+  
+    <title>pl0t</title>
+  
+    <link rel="icon" type="image/png" href="http://files.pl0t.com/view-1/favicon.ico">
+  
+    <!-- PL0T begin, putting it at the end of the page to avoid blocking other content -->
+    <script>
+      window.env = {
+        base_url: "http://files.pl0t.com/view-1"
+      }
+    </script>
+    <link rel="stylesheet" href="http://files.pl0t.com/view-1/releases/2021-08-02-b7e87e/bundle.css">
+    <script defer src="http://files.pl0t.com/view-1/releases/2021-08-02-b7e87e/bundle.js"></script>
+    <!-- PL0T end -->
+  
+  </head>
+  <body>
+  
+  <!-- PL0T data, specify 'json', 'yml' or 'md' formats in the 'type' attribute -->
+  <script id="data" type="{type}">
+  {data}
+  </script>
+  
+  
+  <script>
+    window.on_plot_loaded = function() {
+      window.plot_api.run()
+    }
+  </script>
+  
+  </body>
+  </html>
+  HTML
+  # standalone end
+end
---
+++ api/ruby/readme.md
@@ -0,0 +1,77 @@
+PL0T and **See the Data**
+
+# Example
+
+![](pl0t-screenshot.png)
+
+The example below could be run as:
+
+    ruby play.rb > play.html
+
+It generates the `play.html` notebook page, that could be used locally, or shared on any site.
+
+The code:
+
+```Ruby
+require './pl0t'
+
+
+page = Page.new({
+  title: 'Some page',
+  desc:  'Some description'
+})
+
+
+page.text 'Some text', "
+Some formula $E=mc^2$
+
+Some code `puts 'Hello World'`
+"
+
+
+# Data as TidyData or Array or Column Array
+table_data = [
+  { name: 'Jim Raynor',   age: 30, hp: 250,  is_alive: true },
+  { name: 'Angus Mengsk', age: 50, hp: 100,  is_alive: false },
+  { name: 'Amon',                            is_alive: true }
+]
+page.table 'Some table', table_data, {
+  columns: [
+    { id: :name },
+    { id: :age },
+    { id: :hp, format: { type: :line, ticks: [100] } },
+    { id: :is_alive }
+  ]
+}
+
+
+# Data as TidyData or Array or Column Array
+chart_data = {
+  a: [1, 2, 3,  4, 5],
+  b: [1, 3, 2, -1, 2]
+}
+page.chart 'Some chart', chart_data, [
+  :bar,
+  { x: :a, type: :nominal },
+  { y: :b }
+]
+
+
+# Saving report as HTML file, open it the Browser to see the Notebook
+# You can publish Notebook by copying it to any Web Server
+puts page.to_html
+
+
+# Saving report as HTML file, open it the Browser to see the Notebook
+# You can publish Notebook by copying it to any Web Server
+# page.save 'play.html'
+
+
+# Optionally, you can publish Notebook on the http://pl0t.com site.
+# You would need to get API Token from http://pl0t.com and store it as `plot_api_token` env variable
+# The Notebook will be available as http://al6x.pl0t.com/ruby_test/page.yaml:view
+# page.publish 'http://al6x.pl0t.com/ruby_test/page2.yaml'
+```
+
+For all possible options checkout [online demos](http://pl0t.com) and the
+  [data schema](https://github.com/al6x/pl0t/blob/main/files/view/schema/blocks.ts).
