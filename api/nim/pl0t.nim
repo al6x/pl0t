3d01ab71e7fb67c1522b4a8b811f398913f1634e
alexeypetrushin <alexey.petrushin@gmail.com>
2021-08-23 12:06:21 +0300

    upd

--- api/nim/pl0t.nim
+++ api/nim/pl0t.nim
@@ -1,11 +1,17 @@
 import std/httpclient, std/strformat, std/os, std/tables, std/strutils
-import std/json, std/jsonutils
 
+when (compiles do: import base/[basem, jsonm]):
+  import base/[basem, jsonm]
+else:
+  import std/json, std/jsonutils
+  proc to_json_hook*(n: JsonNode): JsonNode = n
+  template throw(message: string) = raise Exception.new_exception(message)
 
-# Helpers ------------------------------------------------------------------------------------------
-proc to_json_hook*(n: JsonNode): JsonNode = n
-
-template throw(message: string) = raise Exception.new_exception(message)
+template convert_to_json(data: JsonNode, pretty: bool): string =
+  when compiles(data.to_s pretty):
+    data.to_s pretty
+  else:
+    if pretty: data.pretty else: $data
 
 
 # publish, unpublish -------------------------------------------------------------------------------
@@ -16,11 +22,11 @@ proc build_plot_url(url: string): string =
     throw "plot_api_token not defined, define it explicitly or as as `plot_api_token` environment variable"
   fmt"{url}?api_token={plot_api_token}"
 
-proc publish*(data: JsonNode, url: string, pretty = false): void =
+proc publish*(data: JsonNode, url: string, pretty = true): void =
   let url   = build_plot_url url
   let client = new_http_client()
   defer: client.close
-  let json = if pretty: data.pretty else: $data
+  let json = convert_to_json(data, pretty)
   discard client.post_content(url, json)
 
 proc unpublish*(url: string): void =
@@ -97,12 +103,12 @@ proc as_json(page: Page): JsonNode =
   json["page"] = page.page.to_json
   json
 
-proc publish*(page: Page, url: string, pretty = false): void =
+proc publish*(page: Page, url: string, pretty = true): void =
   page.as_json.publish(url, pretty)
 
-proc save*(page: Page, path: string, pretty = false): void =
+proc save*(page: Page, path: string, pretty = true): void =
   let json = page.as_json
-  let json_s = if pretty: json.pretty else: $json
+  let json_s = convert_to_json(json, pretty)
   let html = standalone.replace("{type}", "json").replace("{data}", json_s)
   if file_exists path: remove_file path
   write_file(path, html)
